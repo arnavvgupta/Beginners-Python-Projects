fb009911d9b304ece92fcec0990bcbfdd0b0dd30
ARNAV GUPTA <97589014+arnavvgupta@users.noreply.github.com>
2022-10-22 09:48:57 +0530

    Update scrapping-website.py

--- scrapping-website.py
+++ scrapping-website.py
@@ -6,20 +6,20 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from bs4 import BeautifulSoup
 
-driver = webdriver.Chrome() # You will be required to setup a chrome driver.
+driver = webdriver.Safari() 
 
-driver.get('https://uk.godaddy.com/domainsearch/find?checkAvail=1&domainToCheck=rocket')
+driver.get('https://in.godaddy.com/domainsearch/find?checkAvail=1&domainToCheck=bjmtuc.club')
 
 try:
     WebDriverWait(driver, 5).until(
         EC.presence_of_element_located((By.CLASS_NAME, "spin-results-wrap"))
-    ) # You need to wait for the webpage to render the domains (its an asynchronous web request made).
+    ) 
 except TimeoutException:
     raise
 
 html = driver.page_source
 
-soup = BeautifulSoup(html, "html.parser") # Also note I use the html.parser instead of lxml.
+soup = BeautifulSoup(html, "html.parser") 
 
 domain = soup.find_all('span', class_="domain-name-text h4 text-bold")
 
